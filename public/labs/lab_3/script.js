b85cc65893b7a7ca703f9e21adde10550c366e2b
Chris Ash <christine.ash7@gmail.com>
2021-02-13 11:48:46 -0500

    Added js to allow carousel ability to scroll through images using buttons. Added array function to replace for loop

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,2 +1,33 @@
-/* Put your javascript in here */
-alert("I'm Javascript!")
+'use strict';
+
+function carouselArray() {
+    let arrayList = carousel.querySelectorAll('li')
+    const listContainer = document.createElement('ul')
+    const target = document.querySelector('carousel')
+    target.append(listContainer)
+}
+
+let width = 130; // image width
+let count = 3; // visible images count
+
+let list = carousel.querySelector('ul');
+let listElems = carousel.querySelectorAll('li');
+
+let position = 0; // ribbon scroll position
+
+carousel.querySelector('.prev').onclick = function() {
+    // shift left
+    position += width * count;
+    // can't move to the left too much, end of images
+    position = Math.min(position, 0)
+    list.style.marginLeft = position + 'px';
+};
+
+carousel.querySelector('.next').onclick = function() {
+    // shift right
+    position -= width * count;
+    // can only shift the ribbbon for (total ribbon length - visible count) images
+    position = Math.max(position, -width * (listElems.length - count));
+    list.style.marginLeft = position + 'px';
+};
+
