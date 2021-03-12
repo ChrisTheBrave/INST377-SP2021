538099070b882c5db11f11be647dee16733c0f7c
Chris Ash <christine.ash7@gmail.com>
2021-03-11 20:50:58 -0500

    I broke it lol

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -20,22 +20,51 @@ async function dataHandler(mapObjectFromFunction) {
 //   // and target mapObjectFromFunction to attach markers
   const form = document.querySelector('.userform');
   const search = document.querySelector('#search');
-  const targetList = document.querySelector('.target-list');
   const replyMessage = document.querySelector('.reply-message');
-  // const suggestions = document.querySelector('.suggestions');
+  const suggestions = document.querySelector('.suggestions');
 
   const request = await fetch('/api');
   const data = await request.json();
   // console.table(data);
 
   form.addEventListener('submit', async (event) => {
-    // targetList,innerText = '';
+    suggestions.innerText = '';
 
     event.preventDefault();
     console.log('submit fired', search.value);
     // eslint-disable-next-line max-len
     const filtered = data.filter((record) => record.zip.includes(search.value) && record.geocoded_column_1);
     const topFive = filtered.slice(0, 5);
+
+    if (topFive.length < 1) {
+      replyMessage.classList.add('box');
+      replyMessage.innerText = 'No matches found';
+    } else {
+      console.table(topFive);
+
+      topFive.forEach((item) => {
+        const longLat = item.geocoded_column_1.coordinates;
+        console.log('markerLongLat', longLat[0], longLat[1]);
+        const marker = L.marker([longLat[1], longLat[0]]).addTo(mapObjectFromFunction);
+
+        const appendItem = document.createElement('li');
+        appendItem.classList.add('block');
+        appendItem.classList.add('list-item');
+        appendItem.innerHTML = `<div class="list-header is-size-5">${item.name}</div><address class="is-size-6">${item.address_line_1}</address>`;
+        suggestions.append(appendItem);
+      });
+
+      const {coordinates} = topFive[0]?.geocoded_column_1;
+      console.log('viewSet coords', coordinates);
+      mapObjectFromFunction.panTo([coordinates[1], coordinates[0]], 0);
+    }
+  });
+  search.addEventListener('input', (event) => {
+    console.log('input', (event.target.value));
+    if (search.value.length === 0) {
+      replyMessage.innerText = '';
+      replyMessage.classList.remove('box');
+    }
   });
 }
 
